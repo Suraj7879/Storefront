f89f9012e7a94659ebfcc786cdbe88d6053a27ba
Suraj Singh <surajsingh7346@gmail.com>
2022-04-01 21:51:57 +0530

    Connected project with mysql

--- store/models.py
+++ store/models.py
@@ -12,8 +12,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255) 
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -35,6 +36,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
