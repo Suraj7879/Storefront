1440505dd10c48a1c6c326f4e1e3c84576e52ea0
suraj7879 <surajsingh7346@gmail.com>
2022-04-02 19:06:21 +0530

    Retrieving data from database

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 # Create your views here.
 def wish_birthday(request):
+    # query_set = Product.objects.all()
+    # for product in query_set:
+    #     print(product)
+
+    # try:
+    #     product = Product.objects.get(pk=0)
+    # except:
+    #     pass
+
+    # product = Product.objects.filter(pk=0).first()
+
+    exists = Product.objects.filter(pk=0).exists()
+
     return render(request, 'index.html', {"name": 'Suraj'})
